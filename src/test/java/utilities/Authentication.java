c7eaf8db2bc416a5881af7284ff2394cb8f04e8f
sametozdemir1652 <sametozdemir1652@gmail.com>
2023-06-14 16:56:22 +0300

    first commit

---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import static io.restassured.RestAssured.given;
+
+public class Authentication {
+
+    public static String generateToken(){
+
+       return "";
+    }
+
+}
+
+
+
